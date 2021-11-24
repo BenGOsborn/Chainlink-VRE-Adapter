fe332cbbbf237eba404a50d04c7b608d5efb2720
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-25 10:50:25 +1100

    🎉 Update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import Docker from "dockerode";
+import express from "express";
 
+// Initialize clients
+const app = express();
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
