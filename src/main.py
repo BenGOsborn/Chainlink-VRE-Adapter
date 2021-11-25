f2251d082349c8d7bad3589e331c52b56262d5c9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-26 10:24:16 +1100

    🎉 Update

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
 # The purpose of this file is that I should be able to pass in code and requirements to run, it should execute them in a virtual environment with the dependencies, and should then return the result
+# I can do this from within the single Docker image using Linux Containers I believe
+# On creation of the container, the requirements will be installed, the code will be executed, the result will be returned to the API and will then be returned back to the adapter
+# The adapter has the option to return a given type depending on what the user specifies
