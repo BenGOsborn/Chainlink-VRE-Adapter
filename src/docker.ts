f6efc103c730d59bdd0115bf1d8360629d8d3bf7
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-26 15:06:44 +1100

    🎉 Update

--- src/docker.ts
+++ src/docker.ts
@@ -1,24 +1,49 @@
 import Docker from "dockerode";
 
-// First check if the image exists
+// Declare the versions of Python
+type version = "3.8.12" | "3.9.9" | "3.10.0";
+type versions = {
+    [key in version]: string;
+};
+const VERSIONS: versions = { "3.8.12": "python:3.8.12-alpine3.14", "3.9.9": "python:3.9.9-alpine3.14", "3.10.0": "python:3.10.0-alpine3.14" };
 
+// Utils for Docker
 class DockerUtils {
     docker: Docker;
 
     // Initialize the client
     constructor() {
-        this.docker = new Docker({ socketPath: "/var/run/docker.sock" });
+        this.docker = new Docker({ socketPath: "/var/run/docker.sock" }); // This needs to be exposed to the container it is run it
     }
 
     // Check if the base image exists for the given Python version
-    getImage(version: "3.8.12" | "3.9.9" | "3.10.0") {
-        if (version === "3.8.12") {
-            this.docker.getImage("python:3.8.12-alpine3.14");
-        } else if (version === "3.9.9") {
-            this.docker.getImage("python:3.9.9-alpine3.14");
-        } else if (version === "3.10.0") {
-            this.docker.getImage("python:3.10.0-alpine3.14");
+    async pullImage(version: version) {
+        // Get the list of images
+        const images = await this.docker.listImages();
+        const imageExists = (tag: string) => {
+            const filtered = images.filter((image) => image.RepoTags[0] === tag);
+            return filtered.length > 0;
+        };
+
+        // Pull the version if it does not exist
+        if (version === "3.8.12" && imageExists(VERSIONS[version])) {
+            await this.docker.pull("python:3.8.12-alpine3.14");
+        } else if (version === "3.9.9" && imageExists(VERSIONS[version])) {
+            return this.docker.pull("python:3.9.9-alpine3.14");
+        } else if (version === "3.10.0" && imageExists(VERSIONS[version])) {
+            return this.docker.pull("python:3.10.0-alpine3.14");
         } else {
+            throw Error("This version of Python is not currently supported");
         }
     }
 }
+
+(async function main() {
+    const docker = new DockerUtils();
+    const res = await docker.pullImage("3.8.12");
+    // console.log(res);
+})()
+    .then()
+    .catch((error) => {
+        console.log(error);
+    });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Docker from "dockerode";
 import express from "express";
 import createRequest from "./service";
 
+import "./docker";
+
 // Initialize clients
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 const app = express();
