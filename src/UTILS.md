b3912f69eab462bd845c5f1fb065b95e1ae6fb83
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-27 08:20:15 +1100

    🎉 Update

--- src/UTILS.md
+++ src/UTILS.md
@@ -9,9 +9,6 @@
 -   [HEAVY INSPIRATION](https://github.com/engineer-man/piston/blob/master/packages/python/3.9.4/build.sh)
 -   [Converting Python code to one line](http://jagt.github.io/python-single-line-convert/)
 
--   [If I want TRULY async run code function that supports await and not just a callback](https://stackoverflow.com/questions/44829414/getting-stdout-and-stderr-streams-from-a-docker-container-in-node-js)
--   [Above ^](https://coderedirect.com/questions/539929/wait-for-callback-of-async-function-in-last-stream-ondata-event)
-
 ## Design
 
 -   For every request we will spin up a new Docker image with the base requirements installed WITH the given version (or default version)
--- src/docker.ts
+++ src/docker.ts
@@ -11,7 +11,7 @@ export const VERSIONS: versions = { "3.8.12": "python:3.8.12-alpine3.14", "3.9.9
 // Utils for Docker
 export default class DockerUtils {
     docker: Docker;
-    TIMEOUT: number = 5; // Timeout in seconds
+    TIMEOUT: number = 2; // Timeout in seconds
 
     // Initialize the client
     constructor() {
@@ -32,7 +32,7 @@ export default class DockerUtils {
     }
 
     // Start the Docker image and execute the commands
-    async runCode(version: version, requirements: string[], code: string) {
+    async runCode(version: version, packages: string[], code: string) {
         // Start the container
         const exitIdentifier = crypto.randomBytes(32).toString("hex");
         const container = await this.docker.createContainer({
@@ -45,8 +45,8 @@ export default class DockerUtils {
         // Setup environment
         const streamTimeout = await container.attach({ stream: true, stdout: true, stderr: true });
         const execs = [];
-        const formattedRequirements = requirements.reduce((previous, current) => previous + "\n" + current, "");
-        execs.push(container.exec({ Cmd: ["echo", formattedRequirements, ">", "requirements.txt"], AttachStdin: true, AttachStdout: true }));
+        const formattedPackages = packages.reduce((previous, current) => previous + "\n" + current, "");
+        execs.push(container.exec({ Cmd: ["echo", formattedPackages, ">", "requirements.txt"], AttachStdin: true, AttachStdout: true }));
         execs.push(container.exec({ Cmd: ["pip3", "install", "-r", "requirements.txt"], AttachStdin: true, AttachStdout: true }));
         execs.push(container.exec({ Cmd: ["python3", "-c", code], AttachStdin: true, AttachStdout: true }));
         const toStart = await Promise.all(execs);
@@ -54,8 +54,11 @@ export default class DockerUtils {
 
         // Make a new promise to block the function from exiting and return the data
         const block = new Promise<string>(async (resolve, reject) => {
+            // Record the data by the stream
+            const dataRaw: any[] = [];
+
             // Record timeout message and terminate container
-            streamTimeout.on("data", async () => {
+            streamTimeout.on("data", async (data) => {
                 // Try is needed in case of the container has already stopped
                 try {
                     await container.kill();
@@ -69,15 +72,25 @@ export default class DockerUtils {
             // Execute code and record data
             const streamData = await toStart[2].start({ hijack: true, stdin: true });
             streamData.on("data", async (data) => {
+                // Append the chunk of data to the total data
+                dataRaw.push(data);
+            });
+            streamData.on("end", async () => {
+                // Get the exit code
+                const exitCode = (await toStart[2].inspect()).ExitCode;
+
                 // Try is needed in case of the container has already stopped
                 try {
                     await container.kill();
                     await container.stop();
                 } catch {}
 
-                // Update the return data with success
-                const trimmed = data.toString().trim();
-                resolve(trimmed);
+                // Depending on the exit code reject or resolve the data
+                if (exitCode === 0) {
+                    const data = Buffer.concat(dataRaw).toString();
+                    resolve(data.trim());
+                }
+                reject(`Container exited with exit code ${exitCode}`);
             });
         });
         return await block;
@@ -89,10 +102,9 @@ export default class DockerUtils {
     const dockerUtils = new DockerUtils();
 
     // Test run the code
-    const res = await dockerUtils.runCode("3.8.12", [], "print(3)");
+    const packages: string[] = [];
+    const res = await dockerUtils.runCode("3.8.12", packages, "print(3)");
     console.log(res);
-
-    // **** Tomorrow I need to test with packages and using those packages and multiline code as well
 })()
     .then()
     .catch((error) => {
