f4cf8d688488b7190933945260df22e6b134652f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-26 15:17:43 +1100

    🎉 Update

--- src/docker.ts
+++ src/docker.ts
@@ -1,14 +1,14 @@
 import Docker from "dockerode";
 
 // Declare the versions of Python
-type version = "3.8.12" | "3.9.9" | "3.10.0";
+export type version = "3.8.12" | "3.9.9" | "3.10.0";
 type versions = {
     [key in version]: string;
 };
-const VERSIONS: versions = { "3.8.12": "python:3.8.12-alpine3.14", "3.9.9": "python:3.9.9-alpine3.14", "3.10.0": "python:3.10.0-alpine3.14" };
+export const VERSIONS: versions = { "3.8.12": "python:3.8.12-alpine3.14", "3.9.9": "python:3.9.9-alpine3.14", "3.10.0": "python:3.10.0-alpine3.14" };
 
 // Utils for Docker
-class DockerUtils {
+export default class DockerUtils {
     docker: Docker;
 
     // Initialize the client
@@ -28,6 +28,8 @@ class DockerUtils {
         const filtered = images.filter((image) => image.RepoTags[0] === VERSIONS[version]);
         if (filtered.length === 0) return await this.docker.pull(VERSIONS[version]);
     }
+
+    // Start the Docker image and execute the commands
 }
 
 (async function main() {
