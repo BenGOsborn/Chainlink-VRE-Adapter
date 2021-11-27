e4e14fc5ddaf4d9f6922d4f48f58b374fefdbd29
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-27 12:48:02 +1100

    🎉 Update

--- src/test/docker.test.ts
+++ src/test/docker.test.ts
@@ -1,20 +1,25 @@
-import DockerUtils, { version } from "../docker";
+import DockerUtils, { version, VERSIONS } from "../docker";
 
 (async function main() {
     // Initialize Docker utils
+    console.log("Initializing DockerUtils client");
     const dockerUtils = new DockerUtils({ socketPath: "/var/run/docker.sock" });
 
     // Pull the version
     const version: version = "3.9.9";
+    console.log(`Pulling Python version ${version} with image ${VERSIONS[version]}`);
     await dockerUtils.pullImage(version);
 
     // Run some test code
     const packages: string[] = ["requests==2.22.0"];
     const code = "import requests;print(requests.get('https://www.google.com/'))";
+    console.log(`Installing packages`);
+    packages.forEach((pkg) => console.log(pkg));
+    console.log(`Executing code:\n${code}`);
     const res = await dockerUtils.runCode(version, code, packages);
 
     // Log the result
-    console.log(res);
+    console.log(`Result of executed code:\n${res}`);
 })()
     .then()
     .catch((error) => {
