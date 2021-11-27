fffe9ca889974a6ff698096410a9326b40bb7a07
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-27 19:18:30 +1100

    🎉 Update

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,22 +1,16 @@
 const { Requester, Validator } = require("@chainlink/external-adapter");
 
-// Define custom error scenarios for the API.
-// Return true for the adapter to retry.
-function customError(data: any) {
-    return data.Response === "Error";
-}
-
 // Define custom parameters to be used by the adapter.
 // Extra parameters can be stated in the extra object,
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-    base: ["base", "from", "coin"],
-    quote: ["quote", "to", "market"],
-    endpoint: false,
+    version: ["version", "v"],
+    code: ["code", "executable"],
+    packages: false,
 };
 
-function createRequest(input: any, callback: any) {
+export default function createRequest(input: any, callback: any) {
     // The Validator helps you validate the Chainlink request data
     const validator = new Validator(callback, input, customParams);
     const jobRunID = validator.validated.id;
@@ -26,7 +20,7 @@ function createRequest(input: any, callback: any) {
     const tsyms = validator.validated.data.quote.toUpperCase();
 
     const params = {
-        fsym,
+        fsym, // **** From symbol and to symbol
         tsyms,
     };
 
@@ -42,7 +36,7 @@ function createRequest(input: any, callback: any) {
 
     // The Requester allows API calls be retry in case of timeout
     // or connection failure
-    Requester.request(config, customError)
+    Requester.request(config, (data: any) => {})
         .then((response: any) => {
             // It's common practice to store the desired value at the top-level
             // result key. This allows different adapters to be compatible with
