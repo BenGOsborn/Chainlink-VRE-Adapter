de9a67d87c896048f1d90402298be3e2e95b94a9
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-29 07:58:36 +1100

    🎉 Update

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,7 @@
 # Install the supported Python images **** THIS WILL NEED REFACTORING IN THE MAIN CODE
 docker build -t bengosborn/install . -f install.Dockerfile
-x = (docker run bengosborn/install)
+x=$(docker run bengosborn/install)
 echo $x
+bash -c "${x}"
 
 # Start the service 
