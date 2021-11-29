86a3c57afc929230711786e46ac7144f8ac03ab5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-30 10:45:03 +1100

    🎉 Update

--- node/tunnel.sh
+++ node/tunnel.sh
@@ -1,2 +1,17 @@
 # SSH tunnel into your VPS - replace with your own VPS
-ssh worker@137.184.33.37 -L 6688:localhost:6688 -N -f
+
+# Start or stop the SSH tunnel
+read -p "Decision: start/stop: " decision
+
+case $decision in
+    "start")
+        ssh worker@137.184.33.37 -L 6688:localhost:6688 -N -f
+        ;;
+    
+    "stop")
+        sudo killall ssh
+        ;;
+    
+    *)
+        echo "Unknown command. Valid commands: 'start', 'stop'"
+esac
