919e10f052e44650c8527ae037e80f6298ae5b3d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-30 10:08:59 +1100

    🎉 Update

--- node/start.sh
+++ node/start.sh
@@ -1,14 +1,21 @@
 # Pull and start the node
 # NOTE: If your machine runs out of RAM during the setup, run the setup on another machine, then once the setup is done rerun it on the low RAM machine
-docker pull smartcontract/chainlink:1.0.0
-if [-d ".api"]; then
+
+# Pull the image
+# docker pull smartcontract/chainlink:1.0.0
+
+# Read in the Node credentials
+if [ ! -e ".api" ]; then
     read -p "Your API email: " email
-    email > .api
+    echo $email >> .api
+
     read -p "Your API password: " password
-    password > .api
+    echo $password >> .api
 fi
-if [-d ".password"]; then
+if [ ! -e ".password" ]; then
     read -p "Your wallet password: " wpassword
-    wpassword > .password
+    echo $wpassword >> .password
 fi
-docker run -dp 6688:6688 -v ${PWD}:/chainlink -it --env-file=.env smartcontract/chainlink:1.0.0 local n -p .password -a .api
+
+# Start the node
+# docker run -dp 6688:6688 -v ${PWD}:/chainlink -it --env-file=.env smartcontract/chainlink:1.0.0 local n -p .password -a .api
