99c0376540feeecb0d48cd6711921d96e719f540
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-29 21:42:29 +1100

    🎉 Update

--- node/start.sh
+++ node/start.sh
@@ -1,2 +1,4 @@
+# So apparently IT WORKS - maybe I need to give it admin perms on my other one (run as sudo)
+
 docker pull smartcontract/chainlink:1.0.0
 docker run -p 6688:6688 -v ${PWD}:/chainlink -it --env-file=.env smartcontract/chainlink:1.0.0 local n
