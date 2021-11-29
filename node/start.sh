eb6d094ac3d3c69c8d737317b3d82a7edfe0776e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-30 08:54:37 +1100

    🎉 Update

--- node/start.sh
+++ node/start.sh
@@ -1,4 +1,4 @@
-# So apparently IT WORKS - maybe I need to give it admin perms on my other one (run as sudo)
-
+# Pull and start the node
+# NOTE: If your machine runs out of RAM during the setup, run the setup on another machine, then once the setup is done rerun it on the low RAM machine
 docker pull smartcontract/chainlink:1.0.0
 docker run -p 6688:6688 -v ${PWD}:/chainlink -it --env-file=.env smartcontract/chainlink:1.0.0 local n
---
+++ node/tunnel.sh
@@ -0,0 +1,2 @@
+# SSH tunnel into your VPS - replace with your own VPS
+ssh worker@137.184.33.37 -L 6688:localhost:6688
