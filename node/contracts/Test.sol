237372cb82b72365ecc9cc567b7bf0420db36096
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-30 13:40:01 +1100

    🎉 Update

--- node/contracts/Test.sol
+++ node/contracts/Test.sol
@@ -1,8 +1,20 @@
 pragma solidity 0.6.6;
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Test is ChainlinkClient {
+    address private linkAddress;
+
     constructor(address linkAddress_) {
+        linkAddress = linkAddress_;
         setChainlinkToken(linkAddress_);
+    }
+
+    function callRequest() {
+
+    }
+
+    function withdrawLink() {
+        IERC20(linkAddress).transfer(msg.sender, IERC20(linkAddress).balanceOf(address(this)););
     }
 }
